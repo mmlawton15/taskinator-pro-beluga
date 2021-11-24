60c218025246e3659aedd7f618a3fe0b78031ea7
Mary Margaret <mmlawton15@gmail.com>
2021-11-24 13:03:30 -0500

    finished this lesson

--- js/script.js
+++ js/script.js
@@ -127,6 +127,41 @@ $("#task-form-modal .btn-primary").click(function() {
 
 
 
+
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+});
+
+
+
+
 // due date was clicked
 $(".list-group").on("click", "span", function() {
   // get current text
@@ -146,6 +181,10 @@ $(".list-group").on("click", "span", function() {
   // automatically focus on new element
   dateInput.trigger("focus");
 });
+
+
+
+
 
 
 // remove all tasks
